fe3f4bdde975bb020d911af0478ea087daded5cf
jscardonag <jcardona@tiresia.co>
2022-11-30 08:39:57 -0500

    MCE-248 Ajuste PlantillasCorreo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { NotificacionesController } from './modules/Notificaciones/notificaciones.controller';
 import { NotificacionesResolver } from './modules/Notificaciones/notificaciones.resolver';
 import { NotificacionesService } from './modules/Notificaciones/notificaciones.service';
-import { PlantillasCorreoResolver } from './modules/Plantillas/plantillascorreo.resolver';
-import { PlantillasCorreoService } from './modules/Plantillas/plantillascorreo.service';
-import { PlantillasNotificacionesResolver } from './modules/PlantillasNotificaciones/plantillasnotificaciones.resolver';
-import { PlantillasNotificacionesService } from './modules/PlantillasNotificaciones/plantillasnotificaciones.service';
-import { ProveedoresMensajeriaController } from './modules/ProveedoresMensajeria/proveedoresmensajeria.controller';
+import { NotificacionesPantallaResolver } from './modules/NotificacionesPantalla/notificacionespantalla.resolver';
+import { NotificacionesPantallaService } from './modules/NotificacionesPantalla/notificacionespantalla.service';
+import { PlantillasCorreoResolver } from './modules/PlantillasCorreo/plantillascorreo.resolver';
+import { PlantillasCorreoService } from './modules/PlantillasCorreo/plantillascorreo.service';
+import { PlantillasPantallaResolver } from './modules/PlantillasPantalla/plantillaspantalla.resolver';
+import { PlantillasPantallaService } from './modules/PlantillasPantalla/plantillaspantalla.service';
 import { ProveedoresMensajeriaResolver } from './modules/ProveedoresMensajeria/proveedoresmensajeria.resolver';
 import { ProveedoresMensajeriaService } from './modules/ProveedoresMensajeria/proveedoresmensajeria.service';
-import { ProveedoresMensajeriaParametrosValoresResolver } from './modules/ProveedoresMensajeriaParametrosValores/proveedoresmensajeriaparametrosvalores.resolver';
-import { ProveedoresMensajeriaParametrosValoresService } from './modules/ProveedoresMensajeriaParametrosValores/proveedoresmensajeriaparametrosvalores.service';
+import { ProveedoresMensajeriaParametrosService } from './modules/ProveedoresMensajeriaParametros/proveedoresmensajeriaparametros.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
@@ -30,7 +31,7 @@ import { PrismaService } from './prisma.service';
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
     }),
   ],
-  controllers: [ProveedoresMensajeriaController],
-  providers: [PrismaService, ProveedoresMensajeriaService, ProveedoresMensajeriaResolver, ProveedoresMensajeriaParametrosValoresService, ProveedoresMensajeriaParametrosValoresResolver, PlantillasNotificacionesService, PlantillasNotificacionesResolver, PlantillasCorreoService, PlantillasCorreoResolver, NotificacionesService, NotificacionesResolver],
+  controllers: [NotificacionesController],
+  providers: [PrismaService, ProveedoresMensajeriaService, ProveedoresMensajeriaResolver, ProveedoresMensajeriaParametrosService, ProveedoresMensajeriaParametrosService, PlantillasPantallaService, PlantillasPantallaResolver, PlantillasCorreoService, PlantillasCorreoResolver, NotificacionesPantallaService, NotificacionesPantallaResolver, NotificacionesService, NotificacionesResolver],
 })
 export class AppModule { }
